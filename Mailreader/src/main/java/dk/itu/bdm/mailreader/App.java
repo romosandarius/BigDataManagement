a71da0141d03ed7b18081383050640097c9b692d
sofus <sofusalbertsen@gmail.com>
2014-09-20 22:14:20 +0200

    Changes to be committed: 	modified:   Mailreader/.classpath 	modified:   Mailreader/.settings/org.eclipse.jdt.core.prefs 	new file:   Mailreader/output.txt 	modified:   Mailreader/src/main/java/dk/itu/bdm/mailreader/App.java 	modified:   Mailreader/src/main/java/dk/itu/bdm/mailreader/MailAcc.java

--- Mailreader/src/main/java/dk/itu/bdm/mailreader/App.java
+++ Mailreader/src/main/java/dk/itu/bdm/mailreader/App.java
@@ -3,63 +3,123 @@ package dk.itu.bdm.mailreader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 import javax.mail.*;
 
-public class App {
+public class App extends Thread {
+	private final MailAcc mailAcc;
+	private final Properties props;
+	private ConcurrentHashMap<String, Integer> emailPerAdress;
+	private ConcurrentHashMap<String, ArrayList<Message>> conversations;
+	private final int loglevel;
+
+	public App(MailAcc ma, Properties prop, int loglvl) {
+		mailAcc = ma;
+		loglevel = loglvl;
+		props = prop;
+		emailPerAdress = new ConcurrentHashMap<String, Integer>();
+		System.out.println("initialized thread with " + ma.getAccount());
+	}
+
+	public void run() {
+		try {
+			Session session = Session.getInstance(props, null);
+			Store store = session.getStore();
+			store.connect(mailAcc.getImapAdress(), mailAcc.getAccount(),
+					mailAcc.getPass());
+			int totalCount = 0;
+			Folder[] f = store.getDefaultFolder().list("*");
+			for (javax.mail.Folder folder : f) {
+				try {
+					folder.open(Folder.READ_ONLY);
+					System.out.println(folder.getMessageCount());
+					int n = folder.getMessageCount();
+					if (n > 1) {
+						Message message = folder.getMessage(n);
+						// System.out.println(message.getSubject()+message.getReceivedDate().toString());
+						mailAcc.setLastMail(message.getReceivedDate());
+						message = folder.getMessage(1);
+						// System.out.println(message.getSubject()+message.getReceivedDate().toString());
+						mailAcc.setFirstMail(message.getReceivedDate());
+						Message[] msgs = folder.getMessages();
+						if (loglevel == 2)
+							traverseMessages(msgs);
+
+					}
+					if ((folder.getType() & javax.mail.Folder.HOLDS_MESSAGES) != 0) {
+						totalCount += folder.getMessageCount();
+						// Account for the mails sent
+						if (folder.getFullName().matches(
+								".*[sS]ent.*|.*[sS]end.*|.*[Uu]dbakke.*")) {
+							mailAcc.setMailsSent(folder.getMessageCount());
+							 System.out.println("found the sent folder: "+folder.getFullName()+folder.getMessageCount());
+						}
+					}
+				} catch (MessagingException e) {
+					e.printStackTrace();
+				}
+			}
+			if (loglevel == 2) {
+				ConcurrentHashMap<String, ArrayList<Message>> conv = mailAcc
+						.cleanConversations();
+				System.out.println(mailAcc.getAccount() + " have "
+						+ conv.size() + " converstations:");
+				conv.forEach((k, v) -> System.out.println(k + ":\t" + v.size()));
+			}
+			mailAcc.setMailsTotal(totalCount);
+			// System.out.println("total count= " + totalCount);
+			System.out.println(mailAcc);
+		} catch (Exception mex) {
+			mex.printStackTrace();
+		}
+
+	}
+
 	public static void main(String[] args) throws FileNotFoundException {
-		if(args.length==0){
-			System.out.println("you need to point to your mailcredential file as the argument");
-			System.out.println("the file should be in the order: \"imapadress account password\"");
+		if (args.length == 0) {
+			System.out
+					.println("you need to point to your mailcredential file as the argument");
+			System.out
+					.println("the file should be in the order: \"imapadress account password\"");
 			System.exit(3);
 		}
+		int loglvl;
+
+		loglvl = args.length == 4 ? Integer.parseInt(args[3]) : 1;
 		Properties props = new Properties();
 		props.setProperty("mail.store.protocol", "imaps");
-		Scanner sc = new Scanner(new File(args[0]));		
-		ArrayList<MailAcc> accounts= new ArrayList<MailAcc>();
+		Scanner sc = new Scanner(new File(args[0]));
+		ArrayList<MailAcc> accounts = new ArrayList<MailAcc>();
 		while (sc.hasNext()) {
 			accounts.add(new MailAcc(sc.next(), sc.next(), sc.next()));
 		}
-		System.out.println("Searching through "+accounts.size()+" accounts");
+		System.out
+				.println("Searching through " + accounts.size() + " accounts");
 		for (MailAcc mailAcc : accounts) {
+			new Thread(new App(mailAcc, props, loglvl)).start();
+		}
+		sc.close();
+	}
+
+	public void traverseMessages(Message[] folder) {
+		String from;
+		int msgCount;
+		System.out
+				.println("starting collecting statistics about ppl. This may take a while");
+		int no = 0;
+		for (Message msg : folder) {
 			try {
-				Session session = Session.getInstance(props, null);
-				Store store = session.getStore();
-				store.connect(mailAcc.getImapAdress(),mailAcc.getAccount(),mailAcc.getPass());
-				int totalCount = 0;
-				Folder[] f = store.getDefaultFolder().list("*");
-				for (javax.mail.Folder folder : f) {
-					try {
-						folder.open(Folder.READ_ONLY);
-						int n=folder.getMessageCount();
-						if (n>1){
-						Message message= folder.getMessage(n);
-//						System.out.println(message.getSubject()+message.getReceivedDate().toString());
-						mailAcc.setLastMail(message.getReceivedDate());
-						message=folder.getMessage(1);
-//						System.out.println(message.getSubject()+message.getReceivedDate().toString());
-						mailAcc.setFirstMail(message.getReceivedDate());
-						}
-						if ((folder.getType() & javax.mail.Folder.HOLDS_MESSAGES) != 0) {
-							totalCount += folder.getMessageCount();
-							//Account for the mails sent
-							if(folder.getFullName().matches(".*[sS]ent.*|.*[sS]end.*|.*[Uu]dbakke.*")){
-								mailAcc.setMailsSent(folder.getMessageCount());
-//								System.out.println("found the sent folder: "+folder.getFullName());
-							}
-						}
-					} catch (MessagingException e) {
-						// TODO Auto-generated catch block
-//						e.printStackTrace();
-					}
-				}
-				mailAcc.setMailsTotal(totalCount);
-//				System.out.println("total count= " + totalCount);
-				System.out.println(mailAcc);
-				sc.close();
-			} catch (Exception mex) {
-				mex.printStackTrace();
+				mailAcc.addEmailToConversation(msg);
+				from = msg.getFrom()[0].toString();
+				msgCount = (emailPerAdress.get(from) != null) ? emailPerAdress
+						.get(from) + 1 : 1;
+				emailPerAdress.put(from, msgCount);
+			} catch (ArrayIndexOutOfBoundsException e) {
+			} catch (NullPointerException e) {
+			} catch (MessagingException e) {
 			}
 		}
+
 	}
 }
--- Mailreader/src/main/java/dk/itu/bdm/mailreader/MailAcc.java
+++ Mailreader/src/main/java/dk/itu/bdm/mailreader/MailAcc.java
@@ -1,84 +1,143 @@
 package dk.itu.bdm.mailreader;
 
-import java.util.Date;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Stream;
+
+import javax.mail.Address;
+import javax.mail.Message;
+import javax.mail.MessagingException;
 
 public class MailAcc {
-private String imapAdress,account,pass;
-private Date firstMail,lastMail;
-private int mailsTotal, mailsSent;
+	private final String subRegEx = "RE:? |Re:? |Sv:? |Fwd:? |SV:? |VS:? ";
+	private final String imapAdress, account, pass;
+	private Date firstMail, lastMail;
+	private int mailsTotal, mailsSent;
+	private HashMap<String, Integer> mailsPerAdress;
+	private ConcurrentHashMap<String, ArrayList<Message>> conversations;
+	private ArrayList<Message> tmp;
 
-public Date getFirstMail() {
-	return firstMail;
-}
+	public void addEmailToConversation(Message msg) throws MessagingException,
+			NullPointerException {
+		String cleanSub = msg.getSubject().replaceAll(subRegEx, "");
+		if (conversations.containsKey(cleanSub)) {
+			tmp = conversations.get(cleanSub);
+			tmp.add(msg);
+			// System.out.println(cleanSub+"\t"+tmp.size());
+			conversations.put(cleanSub, tmp);
+		} else {
+			tmp = new ArrayList<Message>();
+			tmp.add(msg);
+			conversations.put(cleanSub, tmp);
+		}
 
-public void setFirstMail(Date firstMail) {
-	if (this.firstMail==null||this.firstMail.after(firstMail)){
-	this.firstMail = firstMail;
 	}
-}
 
-public Date getLastMail() {
-	return lastMail;
-}
-
-public void setLastMail(Date lastMail) {
-	if(this.lastMail==null||this.lastMail.before(lastMail)){
-	this.lastMail = lastMail;
+	public ConcurrentHashMap<String, ArrayList<Message>> cleanConversations() {
+		
+		conversations.forEach((k, v) -> {
+			if (v.size() < 2) {
+				conversations.remove(k);
+				// System.out.println("removed from conversations:" + k);
+			}
+		});
+		 conversations= cleanOneWayCOnversations(conversations);
+		return conversations;
 	}
-}
 
-public int getMailsTotal() {
-	return mailsTotal;
-}
+	private ConcurrentHashMap<String, ArrayList<Message>> cleanOneWayCOnversations(
+			ConcurrentHashMap<String, ArrayList<Message>> conversations2) {
+		System.out.println(conversations2.size());
+		Stream<ArrayList<Message>> trueConversations = conversations2
+				.values()
+				.stream()
+				.filter(m -> {
+					return m.stream()
+							.filter( 
+									message -> {
+										return
+										message.getFrom().stream().filter(from -> from.toString()
+											.equalsIgnoreCase(account).findFirst()
+											.isPresent());
+											});
+				});
+System.out.println(trueConversations.count());
+System.out.println(conversations2.size());
+return conversations2;
+	}
 
-public void setMailsTotal(int mailsTotal) {
-	this.mailsTotal = mailsTotal;
-}
+	public HashMap<String, Integer> getMailsPerAdress() {
+		return mailsPerAdress;
+	}
 
-public int getMailsSent() {
-	return mailsSent;
-}
+	public void setMailsPerAdress(HashMap<String, Integer> collaborations) {
+		// TODO: IMplement merging functionality
+		this.mailsPerAdress = collaborations;
+	}
 
-public void setMailsSent(int mailsSent) {
-	this.mailsSent = mailsSent;
-}
+	public Date getFirstMail() {
+		return firstMail;
+	}
 
-public String getImapAdress() {
-	return imapAdress;
-}
+	public void setFirstMail(Date firstMail) {
+		if (this.firstMail == null || this.firstMail.after(firstMail)) {
+			this.firstMail = firstMail;
+		}
+	}
 
-public void setImapAdress(String imapAdress) {
-	this.imapAdress = imapAdress;
-}
+	public Date getLastMail() {
+		return lastMail;
+	}
 
-public String getAccount() {
-	return account;
-}
+	public void setLastMail(Date lastMail) {
+		if (this.lastMail == null || this.lastMail.before(lastMail)) {
+			this.lastMail = lastMail;
+		}
+	}
 
-public void setAccount(String account) {
-	this.account = account;
-}
+	public int getMailsTotal() {
+		return mailsTotal;
+	}
 
-public String getPass() {
-	return pass;
-}
+	public void setMailsTotal(int mailsTotal) {
+		this.mailsTotal = mailsTotal;
+	}
 
-public void setPass(String pass) {
-	this.pass = pass;
-}
+	public int getMailsSent() {
+		return mailsSent;
+	}
 
-public MailAcc(String imapAdress, String account, String pass) {
-	super();
-	this.imapAdress = imapAdress;
-	this.account = account;
-	this.pass = pass;
-}
+	public void setMailsSent(int mailsSent) {
+		this.mailsSent = mailsSent;
+	}
 
-@Override
-public String toString() {
-	return "MailAcc [imapAdress=" + imapAdress + ", account=" + account
-			+ ", pass=" + pass.hashCode() + ", firstMail=" + firstMail + ", lastMail="
-			+ lastMail + ", mailsTotal=" + mailsTotal + ", mailsSent="
-			+ mailsSent + "]";
-}
+	public String getImapAdress() {
+		return imapAdress;
+	}
+
+	public String getAccount() {
+		return account;
+	}
+
+	public String getPass() {
+		return pass;
+	}
+
+	public MailAcc(String imapAdress, String account, String pass) {
+		super();
+		this.imapAdress = imapAdress;
+		this.account = account;
+		this.pass = pass;
+		mailsPerAdress = new HashMap<String, Integer>();
+		conversations = new ConcurrentHashMap<String, ArrayList<Message>>();
+	}
+
+	@Override
+	public String toString() {
+		return "MailAcc [ imapAdress=" + imapAdress + ", account=" + account
+				+ ", pass=" + pass + ", firstMail=" + firstMail + ", lastMail="
+				+ lastMail + ", mailsTotal=" + mailsTotal + ", mailsSent="
+				+ mailsSent + ", mailsPerAdress=" + mailsPerAdress
+				+ ", conversations=" + conversations.size() + "]";
+	}
 }
